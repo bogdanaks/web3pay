1ad469401bc9e850e9e9ec68d4ab51d52ea3fb9a
bogdanaks <bogdanaks@bk.ru>
2022-12-16 04:04:09 +0300

    Change on Body

--- account-front/src/pages/shop-settings/index.p.tsx
+++ account-front/src/pages/shop-settings/index.p.tsx
@@ -1,23 +1,16 @@
 import React from "react"
-import { Layout } from "shared/ui"
-import { Header, Sidebar } from "widgets"
+import { Body } from "shared/ui"
 
 const ShopSettingsPage = () => {
   return (
-    <Layout>
-      <Header />
-      <div className="flex flex-row h-full">
-        <Sidebar />
-        <div className="p-4">
-          <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-            <div>
-              <div className="text-xl font-medium text-black">ChitChat</div>
-              <p className="text-slate-500">You have a new message!</p>
-            </div>
-          </div>
+    <Body>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
         </div>
       </div>
-    </Layout>
+    </Body>
   )
 }
 

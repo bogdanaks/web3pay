631f99478579bc1873cb0a7cfed59ec0207588a4
bogdanaks <bogdanaks@bk.ru>
2022-12-17 02:13:21 +0300

    add mock data

---
+++ account-front/src/shared/mock/index.ts
@@ -0,0 +1,20 @@
+export const mockNetworks = [
+  {
+    slug: "eth",
+    title: "Ethereum",
+    icon: "/assets/icon-networks/eth.svg",
+    currencies: ["Ethereum", "USDT"],
+  },
+  {
+    slug: "bsc",
+    title: "BSC",
+    icon: "/assets/icon-networks/bsc.svg",
+    currencies: ["BSC", "USDT"],
+  },
+  {
+    slug: "polygon",
+    title: "Polygon",
+    icon: "/assets/icon-networks/polygon.svg",
+    currencies: ["Polygon", "USDT"],
+  },
+]

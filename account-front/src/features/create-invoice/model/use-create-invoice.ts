e71143efd320b71be3d10ec99a88d998d509decc
bogdanaks <bogdanaks@bk.ru>
2022-12-16 20:50:03 +0300

    Create feature model

---
+++ account-front/src/features/create-invoice/model/use-create-invoice.ts
@@ -0,0 +1,44 @@
+import { ElementState } from "app/enums"
+import React from "react"
+import { FieldValues, useForm } from "react-hook-form"
+
+export const useCreateInvoice = () => {
+  const [isVisibleCreate, setIsVisibleCreate] = React.useState(false)
+  const [buttonState, setButtonState] = React.useState(ElementState.normal)
+
+  const { register, handleSubmit } = useForm()
+
+  const onCreate = () => {
+    console.log("create invoice")
+  }
+
+  const onButtonClick = () => {
+    setIsVisibleCreate(true)
+  }
+
+  const onSubmit = async (data: FieldValues) => {
+    // try {
+    //   setButtonState(ElementState.processing)
+    //   await signInEmail({
+    //     email: data.email_input,
+    //     password: data.password_input,
+    //   })
+    //   setButtonState(ElementState.normal)
+    // } catch (err) {
+    //   const error = err as Error
+    //   setServerError(error.message)
+    //   setButtonState(ElementState.disabled)
+    // }
+  }
+
+  return {
+    onCreate,
+    isVisibleCreate,
+    onButtonClick,
+    hideCreateWindow: () => setIsVisibleCreate(false),
+    register,
+    handleSubmit,
+    onSubmit,
+    buttonState,
+  }
+}

3381a223cd42a98119f67248aa9353ea7129717a
bogdanaks <bogdanaks@bk.ru>
2022-12-17 02:13:06 +0300

    change props

--- account-front/src/features/create-invoice/ui/form/currencies.tsx
+++ account-front/src/features/create-invoice/ui/form/currencies.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { FieldValues, UseFormRegister } from "react-hook-form"
+import { mockNetworks } from "shared/mock"
+import { SelectCurrencies } from "widgets"
 
 export const CreateInvoiceFormCurrencies = ({
   register,
@@ -8,7 +10,12 @@ export const CreateInvoiceFormCurrencies = ({
 }) => {
   return (
     <div className="relative z-0 mb-6 w-full group">
-      <label
+      <SelectCurrencies
+        title="Select currencies"
+        placeHolder="Search currency"
+        data={mockNetworks}
+      />
+      {/* <label
         htmlFor="currencies_input"
         className="block mb-2 text-sm font-medium text-gray-900"
       >
@@ -23,7 +30,7 @@ export const CreateInvoiceFormCurrencies = ({
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  outline-1 outline-transparent focus-visible:outline-blue-500 focus-visible:outline-1 block w-full p-2.5"
         placeholder=""
         required
-      ></input>
+      ></input> */}
     </div>
   )
 }

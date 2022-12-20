36d40e3a11fb9b20fcd1d63f030fff45843dbf72
bogdanaks <bogdanaks@bk.ru>
2022-12-20 23:57:55 +0300

    fix

--- server/src/models/invoice/invoice.module.ts
+++ server/src/models/invoice/invoice.module.ts
@@ -1,12 +1,17 @@
 import { Module } from "@nestjs/common"
 import { TypeOrmModule } from "@nestjs/typeorm"
+
+import { InvoicePayments } from "./entities/invoice-payment.entity"
+import { InvoiceProducts } from "./entities/invoice-products.entity"
 import { Invoice } from "./entities/invoice.entity"
 
 import { InvoiceController } from "./invoice.controller"
 import { InvoiceService } from "./invoice.service"
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Invoice])],
+  imports: [
+    TypeOrmModule.forFeature([Invoice, InvoicePayments, InvoiceProducts])
+  ],
   controllers: [InvoiceController],
   providers: [InvoiceService],
   exports: [TypeOrmModule]

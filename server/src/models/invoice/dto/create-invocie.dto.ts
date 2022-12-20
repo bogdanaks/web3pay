5b5921929c9877ba5725f507adfdaab7f08b093b
bogdanaks <bogdanaks@bk.ru>
2022-12-20 23:57:38 +0300

    add validate

---
+++ server/src/models/invoice/dto/create-invocie.dto.ts
@@ -0,0 +1,40 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { Transform } from "class-transformer"
+import { IsEnum, IsOptional, IsString } from "class-validator"
+import { IsBiggerThan } from "src/common/validators/is-bigger-than"
+import { InvoiceTypes } from "../entities/invoice.entity"
+
+export class CreateInvoice {
+  @IsOptional()
+  @IsString()
+  @ApiProperty()
+  order_id?: string
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty()
+  order_description?: string
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty()
+  price_from?: string
+
+  @IsString()
+  @IsBiggerThan("price_from", {
+    message: "price_to must be larger than price_from"
+  })
+  @ApiProperty()
+  price_to: string
+
+  @IsEnum(InvoiceTypes, {
+    message: "type must be DRAFT or INVOICE"
+  })
+  @ApiProperty()
+  @Transform(({ value }: { value: InvoiceTypes }) => value.toLowerCase())
+  type: InvoiceTypes
+
+  @IsString()
+  @ApiProperty()
+  base_currency: string
+}

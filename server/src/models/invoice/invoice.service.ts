aea81ef9e560a937ccd05b4083ea53c621ffee4a
bogdanaks <bogdanaks@bk.ru>
2022-12-21 07:09:02 +0300

    create invoice service update

--- server/src/models/invoice/invoice.service.ts
+++ server/src/models/invoice/invoice.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from "@nestjs/common"
 import { InjectRepository } from "@nestjs/typeorm"
-import { Repository } from "typeorm"
+import { DataSource, Repository } from "typeorm"
 import { Currency } from "../currency/currency.entity"
 import { Invoice, InvoiceTypes } from "./entities/invoice.entity"
+import { InvoicesCurrencies } from "./entities/invoices-currencies.entity"
 
 interface CreateInvoiceProps {
   order_id?: string
   order_description?: string
   price_from?: string
-  currencies_ids?: string[] | number[]
+  currencies_ids?: string[]
   price_to: string
   type: InvoiceTypes
   base_currency: string
@@ -19,7 +20,10 @@ interface CreateInvoiceProps {
 export class InvoiceService {
   constructor(
     @InjectRepository(Invoice)
-    private invoiceRepository: Repository<Invoice>
+    private invoiceRepository: Repository<Invoice>,
+    @InjectRepository(InvoicesCurrencies)
+    private invoiceCurrenciesRepository: Repository<InvoicesCurrencies>,
+    private dataSource: DataSource
   ) {}
 
   async getInvoices(shop_id: number): Promise<Invoice[]> {
@@ -29,18 +33,48 @@ export class InvoiceService {
   async getById(id: number): Promise<Invoice> {
     return await this.invoiceRepository.findOneBy({ id })
   }
-
-  async saveInvoice(invoice: Invoice): Promise<Invoice> {
-    return await this.invoiceRepository.save(invoice)
+  async saveInvoice(data: CreateInvoiceProps): Promise<Invoice> {
+    return await this.invoiceRepository.save(data)
   }
 
   async createInvoice(data: CreateInvoiceProps): Promise<Invoice> {
-    return await this.invoiceRepository.save(data)
+    return await this.invoiceRepository.create(data)
+  }
+
+  async createInvoiceCurrencies({
+    ids,
+    invoice_id
+  }: {
+    ids: string[]
+    invoice_id: number
+  }): Promise<InvoicesCurrencies[]> {
+    return ids.map((id) => {
+      return this.invoiceCurrenciesRepository.create({
+        currency_id: Number(id),
+        invoice_id
+      })
+    })
   }
 
   async createInvoiceWithCurrencies(
     data: CreateInvoiceProps
   ): Promise<Invoice> {
-    return await this.invoiceRepository.save(data)
+    const newInvoice = await this.createInvoice(data)
+
+    return await this.dataSource.transaction(async (manager) => {
+      const txInvoice = await manager.save(newInvoice)
+
+      const newInvoiceCurrencies = await this.createInvoiceCurrencies({
+        ids: data.currencies_ids,
+        invoice_id: txInvoice.id
+      })
+
+      const invoiceCurrencies = await manager.save(newInvoiceCurrencies)
+      await manager.save(invoiceCurrencies)
+      return {
+        ...txInvoice,
+        currencies_ids: data.currencies_ids
+      }
+    })
   }
 }
